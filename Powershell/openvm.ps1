8dc41825bbaf0c165db7d2a2a2e3df58ca879c0a
U-Glados\Ch3LL <Ch3LL@Glados.(none)>
2014-12-18 18:31:20 -0800

    Added powershell script to open virtualbox vm's in windows

---
+++ Powershell/openvm.ps1
@@ -0,0 +1,18 @@
+﻿#Set the environment to the VirtualBox directoy
+$env:Path = "C:\Program Files\Oracle\VirtualBox\";
+
+#Set an array for each vm I want to boo up. This is just the name of the vm in VirtualBox
+$vms = @("git01","web01","salt01","dns01")
+
+
+#Loop through each vm in the array and start the vm
+foreach ($i in $vms) {
+    
+    VBoxManage startvm $i
+
+}
+
+
+
+
+
